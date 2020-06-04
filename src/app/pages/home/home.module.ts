904ffa74ef169dbbb3b442a6678040d4c8930f3e
Sergio Sanchez <sergiosan2802@gmail.com>
2020-06-03 23:54:54 -0500

    implement new methods and Functionality

--- src/app/pages/home/home.module.ts
+++ src/app/pages/home/home.module.ts
@@ -2,14 +2,19 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { HomeRoutingModule } from './home-routing.module';
-
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
 
 @NgModule({
   declarations: [],
   imports: [
     CommonModule,
     HomeRoutingModule,
-    MatIconModule
+    MatIconModule,
+    MatInputModule,
+    ReactiveFormsModule,
+    FormsModule
   ]
 })
 export class HomeModule { }
